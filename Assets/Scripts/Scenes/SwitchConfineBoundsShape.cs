ab479bf8924c787e08637ac3242d70d110ef8a0b
DESKTOP-7P0T49L\Latitude E5420 <howlmoon123@gmail.com>
2020-10-17 10:02:16 -0500

    Cinemachine confiner
    Built confiner and added a script to find it, also fixed ordering layers for scenery.
    Next is scenery fading when walking behind.

---
+++ Assets/Scripts/Scenes/SwitchConfineBoundsShape.cs
@@ -0,0 +1,33 @@
+﻿using Cinemachine;
+using System;
+using UnityEngine;
+
+public class SwitchConfineBoundsShape : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchBoundingShape();
+    }
+
+    /// <summary>
+    /// Switches the confiner that the cinemachine uses to define screen shape.
+    /// </summary>
+    private void SwitchBoundingShape()
+    {
+        //Get polygon collider used by cinemachine
+        PolygonCollider2D polygonCollider2D =  GameObject.FindGameObjectWithTag(Tags.BoundConfiner).GetComponent<PolygonCollider2D>();
+        CinemachineConfiner cinemachineConfiner = GetComponent<CinemachineConfiner>();
+        cinemachineConfiner.m_BoundingShape2D = polygonCollider2D;
+
+        //Special function to clear cache
+        cinemachineConfiner.InvalidatePathCache();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
