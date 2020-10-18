afcbb8191acc76e7b4a22cc09355392d43e1d890
DESKTOP-7P0T49L\Latitude E5420 <howlmoon123@gmail.com>
2020-10-18 10:00:50 -0500

    Item Pickup Script
    Collides with item and gets a item detail

---
+++ Assets/Scripts/Player/ItemPickup.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+       
+        Item item = target.GetComponent<Item>();
+
+        if(item != null)
+        {
+          ItemsDetails details =  InventoryManager.Instance.GetItemsDetails(item.ItemCode);
+            Debug.Log("Item Description " + details.itemDescription);
+        }
+       
+    }
+}
