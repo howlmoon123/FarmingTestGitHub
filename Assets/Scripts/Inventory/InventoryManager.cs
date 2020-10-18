4f3bc08acb2e2461cd5d5a8c9ed8f2b9798925ca
DESKTOP-7P0T49L\Latitude E5420 <howlmoon123@gmail.com>
2020-10-18 09:48:50 -0500

    Inventory Manager 1
    Created Initial Inventory manager script and Game object. Includes a Dictionary to hold game items

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : SingletonMonobehaviour<InventoryManager>
+{
+
+    private Dictionary<int, ItemsDetails> itemDetailsDictionary;
+
+    [SerializeField]
+    private SO_Item itemList = null;
+
+    private void Start()
+    {
+        CreateItemDetailsDictionary();
+    }
+
+    /// <summary>
+    /// Populates Dictionary from SO_Item
+    /// </summary>
+    private void CreateItemDetailsDictionary()
+    {
+        itemDetailsDictionary = new Dictionary<int, ItemsDetails>();
+        foreach(ItemsDetails itemsDetails in itemList.itemsDetails)
+        {
+            itemDetailsDictionary.Add(itemsDetails.itemCode, itemsDetails);
+        }
+
+       
+    }
+    /// <summary>
+    /// Get the item from the Dictionary
+    /// </summary>
+    /// <param name="itemCode"></param>
+    /// <returns></returns>
+    public ItemsDetails GetItemsDetails(int itemCode)
+    {
+        ItemsDetails itemsDetails;
+        if (itemDetailsDictionary.TryGetValue(itemCode, out itemsDetails))
+        {
+            return itemsDetails;
+        }
+        else
+            return null;
+    }
+}
