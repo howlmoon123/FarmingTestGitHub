5303d5bb4225ac738a58bad58c9a708704a273a4
DESKTOP-7P0T49L\Latitude E5420 <howlmoon123@gmail.com>
2020-10-20 12:52:46 -0500

    Competed Swap
    Swap and Swap

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -214,6 +214,23 @@ public class InventoryManager : SingletonMonobehaviour<InventoryManager>
 
     }
 
+    public void SwapInventoryItems(InventoryLocation inventoryLocation, int fromItem, int toItem)
+    {
+        // if fromItem index and toItemIndex are within the bounds of the list, not the same, and greater than or equal to zero
+        if (fromItem < inventoryLists[(int)inventoryLocation].Count && toItem < inventoryLists[(int)inventoryLocation].Count
+             && fromItem != toItem && fromItem >= 0 && toItem >= 0)
+        {
+            InventoryItem fromInventoryItem = inventoryLists[(int)inventoryLocation][fromItem];
+            InventoryItem toInventoryItem = inventoryLists[(int)inventoryLocation][toItem];
+
+            inventoryLists[(int)inventoryLocation][toItem] = fromInventoryItem;
+            inventoryLists[(int)inventoryLocation][fromItem] = toInventoryItem;
+
+            //  Send event that inventory has been updated
+            EventHandler.CallInventoryUpdatedEvent(inventoryLocation, inventoryLists[(int)inventoryLocation]);
+        }
+    }
+
     private void RemoveItemAtPosition(List<InventoryItem> inventoryList, int itemCode, int position)
     {
         InventoryItem inventoryItem = new InventoryItem();
--- Assets/Scripts/UI/UIInventory/UIInventorySlot.cs
+++ Assets/Scripts/UI/UIInventory/UIInventorySlot.cs
@@ -12,6 +12,7 @@ public class UIInventorySlot : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     public GameObject draggedItem;
     [SerializeField] private UIInventoryBar inventoryBar = null;
     [SerializeField] private GameObject itemPrefab = null;
+    [SerializeField] private int slotNumber = 0;
 
     public Image inventorySlotHighlight;
     public Image inventorySlotImage;
@@ -39,7 +40,17 @@ public class UIInventorySlot : MonoBehaviour, IBeginDragHandler, IDragHandler, I
             // If drag ends over inventory bar, get item drag is over and swap them
             if (eventData.pointerCurrentRaycast.gameObject != null && eventData.pointerCurrentRaycast.gameObject.GetComponent<UIInventorySlot>() != null)
             {
-               
+                // get the slot number where the drag ended
+                int toSlotNumber = eventData.pointerCurrentRaycast.gameObject.GetComponent<UIInventorySlot>().slotNumber;
+
+                // Swap inventory items in inventory list
+                InventoryManager.Instance.SwapInventoryItems(InventoryLocation.player, slotNumber, toSlotNumber);
+
+                // Destroy inventory text box
+              //  DestroyInventoryTextBox();
+
+                // Clear selected item
+              //  ClearSelectedItem();
             }
             // else attempt to drop the item if it can be dropped
             else
